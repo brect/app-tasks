b045f4e597bcd728772e7ac99984079b652b3937
brect <b.limas93@gmail.com>
2020-08-30 15:12:49 -0300

    refatorando e ajustando listeners de acoes(alteracao, remocao e ccriacao)

--- app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
+++ app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
@@ -5,8 +5,70 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.tasks.service.listener.ApiListener
+import com.example.tasks.service.listener.ValidationListener
+import com.example.tasks.service.model.PriorityModel
+import com.example.tasks.service.model.TaskModel
+import com.example.tasks.service.repository.TaskRepository
 
 class AllTasksViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mTaskRepository = TaskRepository(application)
+
+    private val mTaskList = MutableLiveData<List<TaskModel>>()
+    var tasks: LiveData<List<TaskModel>> = mTaskList
+
+    private val mValidationListener = MutableLiveData<ValidationListener>()
+    var validation: LiveData<ValidationListener> = mValidationListener
+
+    fun list(){
+        mTaskRepository.getAll(object : ApiListener<List<TaskModel>>{
+            override fun onSuccess(param: List<TaskModel>) {
+                mTaskList.value = param
+            }
+
+            override fun onFailure(msg: String) {
+                mTaskList.value = arrayListOf()
+            }
+
+        })
+    }
+
+    fun complete(id: Int){
+        updateStatus(id, true)
+    }
+
+    fun undo(id: Int){
+        updateStatus(id, false)
+    }
+
+    private fun updateStatus(id: Int, complete: Boolean){
+        mTaskRepository.updateStatus(id, complete, object : ApiListener<Boolean>{
+            override fun onSuccess(param: Boolean) {
+                list()
+                mValidationListener.value = ValidationListener()
+            }
+
+            override fun onFailure(msg: String) {
+                mValidationListener.value = ValidationListener(msg)
+            }
+
+        })
+    }
+
+    fun delete(id: Int) {
+        mTaskRepository.delete(id, object : ApiListener<Boolean>{
+            override fun onSuccess(param: Boolean) {
+                list()
+                mValidationListener.value = ValidationListener()
+            }
+
+            override fun onFailure(msg: String) {
+                mValidationListener.value = ValidationListener(msg)
+            }
+
+        })
+    }
 
 
 }
